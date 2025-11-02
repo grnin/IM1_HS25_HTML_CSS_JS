4ad32e35c182c3455e404e8b74ec4e7c1998890d
Wolfgang Bock <wolfgang.bock@fhgr.ch>
2025-11-02 19:00:01 +0100

    Create script.js

---
+++ 01-html-basics/starter/js/script.js
@@ -0,0 +1,16 @@
+const button = document.querySelector(".primary-button");
+const counterValue = document.querySelector("#counter-value");
+
+let count = 1;
+
+function incrementCounter() {
+  count++; // count = count + 1
+  if (count > 9) {
+    count = 1;
+  }
+  counterValue.textContent = String(count);
+}
+
+button.addEventListener("click", () => {
+  incrementCounter();
+});
